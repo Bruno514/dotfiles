1dcd202516bce3c9eaa6a3bdb894fbe3397067a1
Bruno Silva Oliveira <brunooliveira095@protonmail.com>
2022-06-17 21:04:13 -0300

    new options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -9,5 +9,5 @@ vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.hidden = true
-vim.cmd('set foldmethod=expr')
-vim.cmd('set foldexpr=nvim_treesitter#foldexpr()')
+--vim.cmd('set foldmethod=expr')
+--vim.cmd('set foldexpr=nvim_treesitter#foldexpr()')
